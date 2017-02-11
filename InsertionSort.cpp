d35d209091975335e21df046241648f6753d2569
Michael <michael_tong@outlook.com>
2017-02-11 10:47:40 -0600

    Created the example Insertion Sort code.

---
+++ InsertionSort.cpp
@@ -0,0 +1,60 @@
+/* 
+
+Michael Tong
+InsertionSort.cpp
+
+*/
+
+#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+cout << "This program will sort these numbers using Insertion Sort: " << endl;
+int array[7] = {80, 31, 26, 99, 5, 2, 7};
+int key;
+int i, j;
+
+for (i = 0; i < ARRAY_SIZE(array); i++)
+
+{
+
+cout << array[i] << " ";
+
+}
+
+cout << endl;
+
+for (i = 1; i < ARRAY_SIZE(array); i++)
+
+{
+
+key = array[i];
+j = i - 1;
+
+while ((j >= 0) && (array[j] > key))
+
+{
+
+array[j+1] = array[j];
+j = j - 1;
+array[j+1] = key;
+
+}
+}
+
+cout << "The sorted array is: " << endl; 
+
+for (i = 0; i < ARRAY_SIZE(array); i++)
+
+{
+ 
+cout << array[i] << " ";
+
+}
+}
+
